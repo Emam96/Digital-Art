009faa80844eb78bec75f332da919a1db1d17dfb
Emam Shararah <>
2021-06-03 10:56:15 +0000

    I added a code to welcome the user and color the line by the gender

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var userName = prompt("Enter your name ? ");
+var gender = prompt("Gender?,  you can answer by male or female only ")
+
+if (gender == "male") {document.write("<p style='color:#3d84b8;'> Hello " + userName + " ! </p>")
+ 
+}
+else if (gender == "female") {document.write("<p style='color:#e93b81;'> Hello " + userName + " ! </p>")
+  
+}
+
+
+
+
+// document.write("<p style='color:white;'> Hello " + userName + " ! </p>")
+// document.write("<p style='color:white;'> Hello user ! </p>")
