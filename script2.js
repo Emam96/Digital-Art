0c9edf06c94eb0ad64a327218351c934bf4726d3
Emam Shararah <>
2021-06-06 16:22:58 +0000

    added a while loop to the stars page

--- script2.js
+++ script2.js
@@ -1,12 +1,18 @@
 
 
 
-var siteRate = prompt("Can you rate my site, give me a rate from 0 to 5,"); 
+var siteRate = prompt("Can you rate my site, give me a rate from 1 to 5, you cannot rate by 0, thats rude"); 
 
 
 function s2 () { 
 
   document.write("you rated my site by ");
+while (siteRate != 1 && siteRate != 2  && siteRate != 3 && siteRate != 4 && siteRate != 5 ) 
+{
+
+    siteRate = prompt("please rate the site by 1-5");
+}
+
 for (i = 0; i < siteRate; i++) {
   document.write("★");
 
