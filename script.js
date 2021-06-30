5c464f1d2bdde9f5ffdbf3067361fae9740ed11b
Emam96 <emamsh990@gmail.com>
2021-06-30 16:14:22 +0300

    update

--- script.js
+++ script.js
@@ -1,71 +1,33 @@
-
 var userName = prompt("Enter your name ? ");
-var gender = prompt("what is your gender: male, female,  you can answer by male or female only ");
+var gender = prompt(
+  "what is your gender: male, female,  you can answer by male or female only "
+);
 
+if (userName == "") {
+  userName = "User";
+}
 
-
-if (userName == ""){userName = "User"}
-
-
- function g(){
- 
- 
-  
-
-while (gender != "male" && gender != "female") 
-{
-
+function g() {
+  while (gender != "male" && gender != "female") {
     gender = prompt("please enter one of : male, female");
-  
-
   }
 
-if (gender == "male") {document.write("<p style='color:#0a1931;'> Hello " + userName + " ! </p>")}
+  if (gender == "male") {
+    document.write("<p style='color:#0a1931;'> Hello " + userName + " ! </p>");
+  }
 
- if (gender == "female") {document.write("<p style='color:#cf0000;'> Hello " + userName + " ! </p>")}
+  if (gender == "female") {
+    document.write("<p style='color:#cf0000;'> Hello " + userName + " ! </p>");
+  }
+}
 
- }
-
-
- g();
-
-
-
-
-
-
-
-
+g();
 
 // function img(){
-//   if 
+//   if
 
 // if (siterate == 0) {document.getElementById("stars").document.write()}
 
 //  if (gender == "female") {document.write("<p style='color:#e93b81;'> Hello " + userName + " ! </p>")}
 
-
 // }
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
